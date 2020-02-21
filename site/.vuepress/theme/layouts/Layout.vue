4a1633497e58b7166277c53ae47e818cb68afb9a
monikappv <monika.popova@datopian.com>
2020-02-21 12:29:38 +0200

    [layout][m]: Add main layout template for the website; add if statement for the Home component.

---
+++ site/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Home v-if="$page.frontmatter.layout === 'home'"/>
+  </div>  
+</template>
+
+<script>
+import Home from '../components/Home.vue'
+
+export default {
+  components: { Home },
+
+  computed: {
+    data () {
+      return this.$page.frontmatter
+    },
+
+    actionLink () {
+      return {
+        link: this.data.actionLink,
+        text: this.data.actionText
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+.button-hero {
+  background-color: #505E5E;
+  color: white;
+  border-style: solid;
+  cursor: pointer;
+}
+
+.image-section {
+  background-image: url("/golden-gate-bridge.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+</style>
